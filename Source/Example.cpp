673c2c2091dbec27b2b2ecec6901718314d61dd5
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-05-26 03:38:16 -0300

    Update Example.cpp

--- Source/Example.cpp
+++ Source/Example.cpp
@@ -1,5 +1,5 @@
 
-// INTERNAT PLUGIN de La
+// INTERNAT PLUGIN de Lal
 #include "Example.hpp"          // corresponding header file
 #include <math.h>               // for atan2, sqrt
 #include <stdio.h>            // for sample output
