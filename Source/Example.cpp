eefa3fb3e70139d7ff4fb9da28eef39519635a87
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-06-06 00:24:00 -0300

    Update Example.cpp

--- Source/Example.cpp
+++ Source/Example.cpp
@@ -2,7 +2,7 @@
 // INTERNAL PLUGIN 
 #include "Example.hpp"          // corresponding header file
 #include <math.h>               // for atan2, sqrt
-#include <stdio.h>            // for sample output
+#include <stdio.h>            
 #include <serialport.h>   
 
 
